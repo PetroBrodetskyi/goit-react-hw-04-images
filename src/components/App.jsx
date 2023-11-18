c4f6905b7ae05826485a7a938c921cfbf26db8d8
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2023-11-18 18:02:11 +0100

    refactoringAppModal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { fetchImages } from './servises/api';
 import Searchbar from './Searchbar/Searchbar';
 import Loader from './Loader/Loader';
@@ -9,122 +9,106 @@ import Modal from './Modal/Modal';
 import { Notify } from 'notiflix';
 
 
+const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [per_page] = useState(12);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadMore, setLoadMore] = useState(false);
+  const [error, setError] = useState(null);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-class App extends Component {
-  state = {
-    searchQuery: '',
-    images: [],
-    page: 1,
-    per_page: 12,
-    isLoading: false,
-    loadMore: false,
-    error: null,
-    showModal: false,
-    isModalOpen: false,
-    largeImageURL: 'largeImageURL',
-    id: null,
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      if (!searchQuery) {
+        setIsLoading(false);
+        return;
+      }
+
+      try {
+        const { hits, totalHits } = await fetchImages(searchQuery, page);
+
+        if (hits.length === 0) {
+          Notify.failure('Зображення не знайдено. Спробуйте інший запит.', {
+            position: 'center-bottom',
+            timeout: 5000,
+            width: '290px',
+            fontSize: '18px'
+          });
+          setIsLoading(false);
+          return;
+        }
+
+        setImages((prevImages) => [...prevImages, ...hits]);
+        setLoadMore(page < Math.ceil(totalHits / per_page));
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [searchQuery, page, per_page]);
+
+  const formSubmit = (newSearchQuery) => {
+    const trimmedQuery = newSearchQuery.trim();
+
+    if (trimmedQuery && trimmedQuery !== searchQuery) {
+      setSearchQuery(trimmedQuery);
+      setImages([]);
+      setPage(1);
+      setLoadMore(false);
+      setIsModalOpen(false);
+    }
   };
 
-  componentDidUpdate(_, prevState) {
-    const { searchQuery, page } = this.state;
-    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
-      this.getImages(searchQuery, page);
+  const onloadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+    scrollOnMoreButton();
+  };
+
+  const scrollOnMoreButton = () => {
+    animateScroll.scrollMore(1000, { delay: 10, smooth: 'linear' });
+  };
+
+  const openModal = (newLargeImageURL) => {
+    if (!isModalOpen) {
+      setLargeImageURL(newLargeImageURL);
+      setIsModalOpen(true);
     }
-  }
+  };
 
-  getImages = async (query, page) => {
-  this.setState({ isLoading: true });
-  if (!query) {
-    return;
-  }
-  try {
-    const { hits, totalHits } = await fetchImages(query, page);
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-    if (hits.length === 0) {
-      Notify.failure('Зображення не знайдено. Спробуйте інший запит.', {
-        position: 'center-bottom',
-        timeout: 5000,
-        width: '290px',
-        fontSize: '18px'
-      });
-      return;
-    }
+  return (
+    <>
+      {!isModalOpen && <Searchbar onSubmit={formSubmit} />}
 
-    this.setState((prevState) => ({
-      images: [...prevState.images, ...hits],
-      loadMore: page < Math.ceil(totalHits / this.state.per_page),
-    }));
-  } catch (error) {
-    this.setState({ error: error.message });
-  } finally {
-    this.setState({ isLoading: false });
-  }
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ImageGallery images={images} openModal={openModal} />
+      )}
+      {error && <p>❗something went wrong❗</p>}
+
+      {loadMore && !isLoading && <Button onloadMore={onloadMore} page={page} />}
+
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          onClose={closeModal}
+          largeImageURL={largeImageURL}
+        />
+      )}
+    </>
+  );
 };
-
-  formSubmit = (searchQuery) => {
-  const trimmedQuery = searchQuery.trim();
-
-  if (trimmedQuery && trimmedQuery !== this.state.searchQuery) {
-    this.setState({
-      searchQuery: trimmedQuery,
-      images: [],
-      page: 1,
-      loadMore: false,
-    });
-  }
-};
-
-  onloadMore = () => {
-  this.setState((prevState) => ({ page: prevState.page + 1 }));
-  this.scrollOnMoreButton();
-};
-
-  scrollOnMoreButton = () => {
-  animateScroll.scrollMore(1000, { delay: 10, smooth: 'linear' });
-};
-
-  openModal = (largeImageURL) => {
-  if (!this.state.showModal) {
-    this.setState({
-      showModal: true,
-      largeImageURL: largeImageURL,
-      isModalOpen: true,
-    });
-  }
-};
-
-  closeModal = () => {
-  this.setState({
-    showModal: false,
-    isModalOpen: false,
-  });
-};
-
-  render() {
-    const { images, isLoading, loadMore, page, showModal, largeImageURL, isModalOpen } =
-      this.state;
-    return (
-      <>
-        {!isModalOpen && <Searchbar onSubmit={this.formSubmit} />}
-
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <ImageGallery images={images} openModal={this.openModal} />
-        )}
-
-        {loadMore && !isLoading && (<Button onloadMore={this.onloadMore} page={page} />
-        )}
-        {showModal && (
-          <Modal
-            isOpen={showModal}
-            onClose={this.closeModal}
-            largeImageURL={largeImageURL}
-          />
-        )}
-      </>
-    );
-  }
-}
 
 export default App;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,48 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Modal from 'react-modal';
 import { AiOutlineClose } from 'react-icons/ai';
 import css from './Modal.module.css';
 
-
-
-class ModalWindow extends Component {
-  handleKeyDown = (e) => {
+const ModalWindow = ({ isOpen, onClose, largeImageURL }) => {
+  const handleKeyDown = (e) => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackDropClick = (e) => {
+  const handleBackDropClick = (e) => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return (
-      <Modal
-        isOpen={true}
-        onRequestClose={this.props.onClose}
-        contentLabel="Modal"
-        className={css.modal}
-        style={{
-          content: {
-            margin: 'auto',
-          },
-        }}
-      >
-        
-        <div className={css.overlay} onClick={this.handleBackDropClick}>
-          <div className={css.modal}>
-            <img className={css.imagemodal} src={this.props.largeImageURL} alt="" />
-            <button className={css.closeButton} onClick={this.props.onClose}>
-              <AiOutlineClose size={22} />
-            </button>
-          </div>
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      contentLabel="Modal"
+      className={css.modal}
+      style={{
+        content: {
+          margin: 'auto',
+        },
+      }}
+      onAfterOpen={() => document.addEventListener('keydown', handleKeyDown)}
+      onAfterClose={() => document.removeEventListener('keydown', handleKeyDown)}
+    >
+      <div className={css.overlay} onClick={handleBackDropClick}>
+        <div className={css.modal}>
+          <img className={css.imagemodal} src={largeImageURL} alt="" />
+          <button className={css.closeButton} onClick={onClose}>
+            <AiOutlineClose size={22} />
+          </button>
         </div>
-      </Modal>
-    );
-  }
-}
+      </div>
+    </Modal>
+  );
+};
 
 export default ModalWindow;
